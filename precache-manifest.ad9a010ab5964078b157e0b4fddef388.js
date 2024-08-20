ca742bab34989b75c21804fbe6ce861a77959cfb
Reneta <connect@reneta.nl>
2024-08-20 13:57:38 +0200

    Updates

---
+++ precache-manifest.ad9a010ab5964078b157e0b4fddef388.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4397f59e94c2b4e8efa507fced62ca70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25bf82ac1a796a2f1a66",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "f0cc3dc7c46c2f4e1f10",
+    "url": "/static/css/main.c23375db.chunk.css"
+  },
+  {
+    "revision": "25bf82ac1a796a2f1a66",
+    "url": "/static/js/2.a07d0712.chunk.js"
+  },
+  {
+    "revision": "9fd8ac69f8779f68b807eb06f9423c10",
+    "url": "/static/js/2.a07d0712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cc3dc7c46c2f4e1f10",
+    "url": "/static/js/main.17b827d5.chunk.js"
+  },
+  {
+    "revision": "59f929696375951947d5",
+    "url": "/static/js/runtime-main.29a00d74.js"
+  },
+  {
+    "revision": "ecbc3db06824e4a44b16732ac7af6ef6",
+    "url": "/static/media/AVO15349.ecbc3db0.JPG"
+  }
+]);
